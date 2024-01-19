ab80296d0d638696d93e3c3d5ca731fa426698d8
Admin <admin@Admins-MacBook-Air.local>
2024-01-19 12:05:05 +0000

    more typescript katas

--- __tests__/kata1.test.ts
+++ __tests__/kata1.test.ts
@@ -1,7 +1,21 @@
-import {add} from '../src/kata1';
+import {stat} from '../src/kata1';
 
-describe('Maths functions', () => {
-    test('should add two numbers correctly', () => {
-        expect(add(1,2)).toBe(3);
+describe('stat function', () => {
+    
+    test('if string is empty, return empty string', () => {
+        expect(stat('')).toBe('');
+    }) 
+    
+    test('if all the times are 00|00|00, return Range:00|00|00 Average: 00|00|00 Median: 00|00|00', () => {
+        expect(stat('00|00|00, 00|00|00, 00|00|00, 00|00|00')).toBe('Range: 00|00|00 Average: 00|00|00 Median: 00|00|00');
     })
+
+    test('function works for general case', () => {
+        expect(stat('01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17')).toBe('Range: 01|01|18 Average: 01|38|05 Median: 01|32|34')
+
+        expect(stat("02|15|59, 2|47|16, 02|17|20, 2|32|34, 2|32|34, 2|17|17")).toBe("Range: 00|31|17 Average: 02|27|10 Median: 02|24|57")
+    })
+
 })
+
+
--- src/kata1.ts
+++ src/kata1.ts
@@ -1,3 +1,52 @@
-export function add(a: number, b:number) : number {
-    return a + b
+export const stat = (s: string): string => {
+
+    let range: number = 0;
+    let median: number = 0;
+    let mean: number = 0;
+
+    if(s === '') {
+        return ''
+    }
+
+    function Time(secondsTime: number) { 
+        let hours : string = Math.trunc(secondsTime/3600).toString()
+        let mins : string = Math.trunc((secondsTime - +hours*3600)/60).toString()
+        let secs: string = Math.trunc(secondsTime - +hours*3600 - +mins*60).toString()
+
+        if(hours.length === 1) {
+            hours = '0' + hours
+        }
+        if(mins.length === 1) {
+            mins = '0' + mins
+        }
+        if(secs.length === 1) {
+            secs = '0' + secs
+        }
+
+        return hours + '|' + mins + '|' + secs;
+
+    }
+
+    const arrOfTimes : string[] = s.split(', ')
+
+    const times: string[][] = arrOfTimes.map((time) => time.split('| '))
+
+    const timesSplit: string[][] = times.map((timeArr) => timeArr[0].split('|'))
+
+    const secsArr: number[] = timesSplit.map((timeSplit) => +timeSplit[0]*3600 + +timeSplit[1]*60 + +timeSplit[2])
+
+    const orderedTimes: number[] = secsArr.sort((a,b) => a-b)
+
+    range = orderedTimes[orderedTimes.length - 1] - orderedTimes[0]
+
+    if(orderedTimes.length % 2 === 0) {
+        median = (orderedTimes[(orderedTimes.length) / 2] + orderedTimes[(orderedTimes.length) / 2 - 1]) / 2
+    } else{
+        median = orderedTimes[(orderedTimes.length) / 2 - 0.5]
+    }
+
+    mean = orderedTimes.reduce((curr,acc) => curr + acc) / orderedTimes.length
+
+    return 'Range: ' + Time(range) + ' Average: ' + Time(mean) + ' Median: ' + Time(median)
+
 }
